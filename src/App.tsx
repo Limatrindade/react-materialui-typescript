658185c71303f0d453c4e7fff23f8c480d3bc651
Matheus Lima <teulima20@hotmail.com>
2023-07-21 20:42:45 -0300

    feat: configuring theme in mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
-import { LightTheme } from "./shared/themes";
-import { ThemeProvider } from '@mui/material';
+import { AppThemeProvider } from "./shared/contexts";
 
 function App() {
   return (
-    <ThemeProvider theme={LightTheme}>
-      <BrowserRouter>
-        <AppRoutes/>
-      </BrowserRouter>
-    </ThemeProvider>
+      <AppThemeProvider>
+        <BrowserRouter>
+          <AppRoutes/>
+        </BrowserRouter>
+      </AppThemeProvider>
   );
 }
 
