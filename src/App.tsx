9628851e8d348aa4fe1f135e60ebce2ee372e016
Matheus Lima <teulima20@hotmail.com>
2023-07-20 23:10:31 -0300

    feat: added light theme and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
+import { LightTheme } from "./shared/themes";
+import { ThemeProvider } from '@mui/material';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes/>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
