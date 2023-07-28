96be58a12913bbb86669666cb94d4bca234a0a30
Matheus Lima <teulima20@hotmail.com>
2023-07-27 22:47:52 -0300

    fix: adjustments in theme modes

--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -1,36 +1,39 @@
 import { Box } from '@mui/system';
 import { Typography, useTheme, IconButton, Icon, useMediaQuery } from '@mui/material';
 import { useDrawerContext } from '../contexts';
+import { ReactNode } from 'react';
 
 interface ILayoutBaseDePagina {
   titulo: string;
+  barraDeFerramentas?: ReactNode;
 }
 
-export const LayoutBaseDePagina: React.FC<ILayoutBaseDePagina> = ({ children, titulo }) => {
+export const LayoutBaseDePagina: React.FC<ILayoutBaseDePagina> = ({ children, barraDeFerramentas, titulo }) => {
     
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+  const mdDown = useMediaQuery(theme.breakpoints.down('md'));
 
   const { toggleDrawerOpen } = useDrawerContext();
 
   return (
     <Box height="100%" display="flex" flexDirection="column" gap={1}>
-      <Box padding={1} display="flex" alignItems="center" height={theme.spacing(12)}>
+      <Box padding={1} display="flex" alignItems="center" height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}>
 
         {smDown && (<IconButton onClick={toggleDrawerOpen}>
           <Icon>menu</Icon>
         </IconButton>)}
 
-        <Typography variant='h5'>
+        <Typography variant={ smDown ? 'h5' : mdDown ? 'h4' : 'h3' } whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis">
           {titulo}
         </Typography>
       </Box>
 
-      <Box>
-        Barra de ferramentas
-      </Box>
+      {barraDeFerramentas && (<Box>
+        {barraDeFerramentas}
+      </Box>)}
 
-      <Box>
+      <Box flex={1} overflow="auto">
         {children}
       </Box>
     </Box>
